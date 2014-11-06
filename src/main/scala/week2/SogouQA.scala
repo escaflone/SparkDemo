3d3550405a3ca63dc0b4cff94892ba38d873549b
binwu4 <binwu4@ebay.com>
2014-11-06 11:31:22 +0800

    init

---
+++ src/main/scala/week2/SogouQA.scala
@@ -0,0 +1,26 @@
+package week2
+
+import org.apache.spark.SparkContext._
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SogouQA {
+  def main(args: Array[String]) {
+    if (args.length == 0) {
+      System.err.println("Usage: SogouQA <file1> <file2>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf().setAppName("SogouQA")
+    val sc = new SparkContext(conf)
+
+
+    sc.textFile(args(0)).map(_.split("\t"))
+      .filter(_.length==6).map(x=>(x(1),1))
+      .reduceByKey(_+_).map(x=>(x._2,x._1))
+      .sortByKey(false).map(x=>(x._2,x._1))
+      .saveAsTextFile(args(1))
+
+
+    sc.stop()
+  }
+}
---
+++ src/main/scala/week2/test.scala
@@ -0,0 +1,10 @@
+package week2
+
+/**
+ * Created by clat on 14-11-5.
+ */
+object test {
+  def main(args: Array[String]) {
+    println(123)
+  }
+}
